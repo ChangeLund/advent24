6d1e0802290629ee4ec4dd8a39227a3f83fc20be
Endre Lund <endreelund@gmail.com>
2024-12-01 11:41:39 +0100

    init commit

---
+++ advent24/Days/1/Day1.cs
@@ -0,0 +1,13 @@
+﻿namespace advent24
+{
+    public class Day1
+    {
+        public static void SolveDay1()
+        {
+            Console.WriteLine("Enter some input for me please:");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+
+    }
+}
---
+++ advent24/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace advent24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Day1.SolveDay1(); 
+        }
+    }
+}
