ae98d65cb4965e89c7296285ad6f76e5d07a057a
Endre <endre.lund@capgemini.com>
2024-12-02 13:43:02 +0100

    Day 2 - ⭐⭐

--- advent24/Days/2/Day2.cs
+++ advent24/Days/2/Day2.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Diagnostics.Metrics;
+using System.Linq;
 
 namespace advent24;
 
@@ -10,46 +11,73 @@ public class Day2
         var input = FileReaderUtil.ReadFile("C:\\Users\\enlund\\source\\repos\\advent24\\advent24\\Days\\2\\input.txt");
         var testInput = new List<string> { "7 6 4 2 1", "1 2 7 8 9", "9 7 6 2 1", "1 3 2 4 5", "8 6 4 4 1", "1 3 6 7 9" };
         Part1(input);
-
+        Part2(testInput);
     }
 
     internal static void Part1(List<string> input)
     {
+        Console.WriteLine("Solving Part1:");
         var splittedInput = input.Select(c => c.Split(' ')).Select(x => x.Select(int.Parse).ToList()).ToList();
 
-        var sum = splittedInput.Select(c =>
-        {
-            var increasing = true;
-            var decreasing = true;
+        var sum = splittedInput.Count(c=> CountSafeFromList(c));
 
-            for (int i = 0; i < c.Count - 1; i++)
-            {
-                var diff = c[i + 1] - c[i];
-                if (diff > 3 || diff < -3 || diff == 0)
-                {
-                    increasing = false;
-                    decreasing = false;
-                    break;
-                }
-                if (diff < 0)
-                {
-                    increasing = false;
-                }
-                if (diff > 0)
-                {
-                    decreasing = false;
-                }
-            }
-
-            return (increasing || decreasing) ? 1 : 0;
-        }).Sum();
-
-        Console.WriteLine($"Total Sum: {sum}");
+        Console.WriteLine($"Total Sum:{sum}");
     }
 
-    internal static void Part2() 
+    internal static void Part2(List<string> input)
     {
-        
+        Console.WriteLine("Solving Part2:");
+        var splittedInput = input.Select(c => c.Split(' ')).Select(x => x.Select(int.Parse).ToList()).ToList();
 
+        var sum = splittedInput.Count(c => IsSafeWithDamper(c));
+
+        Console.WriteLine($"Sum Part 2: {sum}");
+    }
+
+    internal static bool IsSafeWithDamper(List<int> list)
+    {
+        if (CountSafeFromList(list))
+        {
+            return true;
+        }
+
+        for (int i = 0; i < list.Count; i++)
+        {
+            var tempList = new List<int>(list);
+            tempList.RemoveAt(i);
+            if (CountSafeFromList(tempList))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    internal static bool CountSafeFromList(List<int> list)
+    {
+        var increasing = true;
+        var decreasing = true;
+
+        for (int i = 0; i < list.Count - 1; i++)
+        {
+            var diff = list[i + 1] - list[i];
+            if (diff > 3 || diff < -3 || diff == 0)
+            {
+                increasing = false;
+                decreasing = false;
+                break;
+            }
+            if (diff < 0)
+            {
+                increasing = false;
+            }
+            if (diff > 0)
+            {
+                decreasing = false;
+            }
+        }
+
+        return increasing || decreasing;
     }
 }
---
+++ advent24/Days/3/Day3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent24;
+
+public class Day3
+{
+    internal static void SolveDaSolveDay3 ()
+    {
+
+    }
+
+    internal static void Part1()
+    {
+
+    }
+
+    internal static void Part2() 
+    {
+
+    }
+
+}
