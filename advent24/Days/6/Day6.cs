4a767ac30ffd1c6c049f90629b67ff463abded87
Endre <endre.lund@capgemini.com>
2024-12-05 10:07:57 +0100

    Ready for day 6

---
+++ advent24/Days/6/Day6.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace advent24;
+
+public class Day6
+{
+
+    public static void SolveDay6()
+    {
+        var testInput = FileReaderUtil.ReadFile("");
+        var input = FileReaderUtil.ReadFile("");
+
+        Part1(testInput);
+        Part2(testInput);
+    }
+
+
+
+    internal static void Part1(List<string> input)
+    {
+
+
+    }
+
+    internal static void Part2(List<string> input)
+    {
+
+
+    }
+}
--- advent24/Program.cs
+++ advent24/Program.cs
@@ -11,7 +11,8 @@ namespace advent24
             //Day2.SolveDay2();
             //Day3.SolveDay3();
             //Day4.SolveDay4();
-            Day5.SolveDay5();
+            //Day5.SolveDay5();
+            Day6.SolveDay6();
         }
     }
 }
